61fa471478c26f48efbd836ed5f8761de2aa62e6
Ahmed Ikram <ikramahmed398@gmail.com>
2021-11-16 17:34:32 +0400

    Init create-react frontend and fixed bugs in backend

--- backend/react_blog/blog/api/views.py
+++ backend/react_blog/blog/api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from blog.models import Blog
 from blog.api.serializers import BlogSerializer
 
-class BlogView(viewsets.ModelViewSet):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer(queryset, many=True)
+class BlogView(viewsets.ViewSet):
+
+      def list(self, request):
+        queryset = Blog.objects.all()
+        serializer = BlogSerializer(queryset, many=True)
+        return Response(serializer.data)
+        
